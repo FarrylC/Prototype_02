381144dd373fefebc0826a0e3e2e9db7de3eff96
FarrylC <changfa@sheridancollege.ca>
2020-10-19 13:04:31 -0400

    Implement enemy movement
    Implement enemy movement, allowing the enemy to move up and down continuously at a constant speed.

---
+++ Prototype_02/Assets/Scripts/Controllers/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float distance;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Start movement cycle
+        StartCoroutine(ChangeDirection());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move forward
+        transform.Translate(gameObject.transform.up * speed * Time.deltaTime, Space.World);
+    }
+
+    // Reverse travel direction
+    IEnumerator ChangeDirection()
+    {
+        yield return new WaitForSeconds(distance / speed);
+
+        gameObject.transform.up *= -1;
+        StartCoroutine(ChangeDirection());
+    }
+}
