f3ee0dab84ff5417809e9fd66e023435e880f539
ZizoMass <zmass@zmass.productions>
2020-10-20 01:31:42 -0300

    Asteroids own rotation
    The individual rotation of the steroids on their own axes has been completed.
    The rotation script consists of two checkboxes available on the interface, where it is allowed to configure, without the need to open the script, the individual rotation of each prefab, as well as its direction.
    The natural rotation of the asteroids was also programmed to be generated in a minimum random space close to the number of speed informed, making the rotation of several asteroids on the screen more random/caotic and natural.

--- Prototype_02/Assets/Scripts/Components/Asteroid_movement.cs
+++ Prototype_02/Assets/Scripts/Components/Asteroid_movement.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Asteroid_movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float rotationSpeed = 3f;
+    public bool clockwise = true;
+    private float realRotationSpeed;
+
+
+     void Start()
     {
-        
+        realRotationSpeed = (Random.Range(rotationSpeed - 3f, rotationSpeed + 3f));
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        float rotAmount;
+        if (clockwise)
+        { 
+            rotAmount = realRotationSpeed * Time.deltaTime; 
+        }
+        else
+        {
+            rotAmount = realRotationSpeed * -Time.deltaTime;
+        }
+
+        float curRot = transform.localRotation.eulerAngles.z;
+        transform.localRotation = Quaternion.Euler(new Vector3(0, 0, curRot + rotAmount));
     }
 }
